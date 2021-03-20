abba67fc138474d69ab8ae69a5b3b12afb4e4e5d
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-20 09:35:49 +0900

    commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@gmail.com</dd>
+        <dt>Tel</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>Scompany</dd>
+        <dt>Web</dt>
+        <dd>00.com</dd>
+      </dl>
+    </div>
+  );
+};
