97772392a653bb31a7f18792810e434711af7fef
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-20 22:57:13 +0900

    global context

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
+import React, { useContext } from "react";
+import { UserContext } from "../../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
   const { image, name } = props;
+  const { userInfo } = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
   return (
     <SContainer>
       <SImage height={160} width={160} alt="プロフィール" src={image} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
 };
@@ -22,4 +28,9 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
