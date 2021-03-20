a1a2530c591d77fd1116eb13e5e132cf213e6d94
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-20 19:56:30 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "taeyong",
-  image: "https://source.unsplash.com/brFsZ7qszSY",
-  email: "ppp@gmail.com",
-  phone: "000-0000-0000",
-  company: {
-    name: "tCompany"
-  },
-  website: "sss.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>検索</PrimaryButton>
-        <SecondaryButton>テスト</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
